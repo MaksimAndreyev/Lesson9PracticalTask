7587fbf85706d1e0c00b28df6faa61c6829755e4
maximandreev <maxands@yandex.ru>
2022-09-04 16:23:07 +0300

    Task 2 completed

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int Sum(int M, int N, int sum)
+{
+    if (M == N)
+    {
+        return M;
+    }
+    sum += N + Sum(M, N-1, sum);
+    return sum;
+}
+
+
+Console.WriteLine("Введите число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(M, N, 0));
