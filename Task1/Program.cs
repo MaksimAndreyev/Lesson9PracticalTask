c65d7bf53411ae9b9333ece1f06dffdb42ec9702
maximandreev <maxands@yandex.ru>
2022-09-04 16:16:24 +0300

    Task 1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int Numbers(int N)
+{
+    if (N == 1)
+    {
+        return N;
+    }
+    Console.Write(N+", ");
+    N = Numbers(N-1);
+    return N;
+}
+
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Numbers(N));
