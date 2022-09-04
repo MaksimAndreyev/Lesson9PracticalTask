9bb27a0ea231135b5b6f23f5212f881669d3f73d
maximandreev <maxands@yandex.ru>
2022-09-04 16:38:41 +0300

    Task 3 completed

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n+1;
+    }
+    if (n == 0)
+    {
+        return Ackermann(m-1, 1);
+    }
+    return Ackermann(m-1, Ackermann(m, n-1));
+}
+
+
+Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Ackermann(m, n));
